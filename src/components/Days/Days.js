02b4ca6f6ca54b782ccb8e609b06e98328a39f4d
mtaylorpdx <taylor.matt@protonmail.com>
2020-04-21 12:04:03 -0700

    add class Days with render
    co-authored-by: dusty mccord <dustymccord@gmail.com>
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>

--- src/components/Days/Days.js
+++ src/components/Days/Days.js
@@ -4,40 +4,60 @@ import Photo from "./Photo";
 import "./Days.css";
 import marketSchedule from "../Data/MarketSchedule.js";
 
-function Days() {
-  const getDetails = (dayString) => {
-    let result;
+class Days extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = marketSchedule[0];
+  }
+  // const getDetails = (dayString) => {
+  //   let result;
 
-    marketSchedule.forEach((obj) => {
-      if (dayString === obj.day) {
-        result = obj;
-      }
-    });
-    return result;
-  };
+  //   marketSchedule.forEach((obj) => {
+  //     if (dayString === obj.day) {
+  //       result = obj;
+  //     }
+  //   });
+  //   return result;
+  // };
 
-  let data = getDetails("Tuesday");
-  return (
-    <React.Fragment>
-      <div className="days-box">
-        <div className="days-row">
-          <h4>Days</h4>
-          <p>Monday</p>
-          <p>Tuesday</p>
-          <p>Thursday</p>
-          <p>Friday</p>
-          <p>Saturday</p>
-          <p>Sunday</p>
+  // let data =   {
+  //   day: "Sunday",
+  //   location: "Lents International",
+  //   hours: "9:00am - 2:00pm",
+  //   booth: "4A"
+  // };
+  // const handleClick = (dayString) => {
+  //   const fromArray = getDetails(dayString);
+  //   let data = fromArray;
+  // };
+  render() {
+    return (
+      <React.Fragment>
+        <div className="days-box">
+          <div className="days-row">
+            <h4>Days</h4>
+            <p>{/* <button onClick={handleClick("Monday")}>Monday</button> */}</p>
+            <p>Tuesday</p>
+            <p>Thursday</p>
+            <p>Friday</p>
+            <p>Saturday</p>
+            <p>Sunday</p>
+          </div>
+          <div className="days-row">
+            <Location
+              day={this.state.day}
+              hours={this.state.hours}
+              location={this.state.location}
+              booth={this.state.booth}
+            />
+          </div>
+          <div className="days-row">
+            <Photo />
+          </div>
         </div>
-        <div className="days-row">
-          <Location day={data.day} hours={data.hours} location={data.location} booth={data.booth} />
-        </div>
-        <div className="days-row">
-          <Photo />
-        </div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 
 export default Days;
